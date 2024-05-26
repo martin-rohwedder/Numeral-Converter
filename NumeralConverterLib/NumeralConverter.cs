9848c967111abae700df0077577043eae3a302ff
Martin Rohwedder <m.rohwedder@gmail.com>
2024-05-26 15:16:30 +0200

    To Roman Numeral conversion added with unit test

--- NumeralConverterLib/NumeralConverter.cs
+++ NumeralConverterLib/NumeralConverter.cs
@@ -2,5 +2,33 @@
 {
     public class NumeralConverter
     {
+        private readonly List<(int Number, string RomanSymbol)> _romanSymbolsList = new List<(int Number, string RomanSymbol)>
+        {
+            (1000, "M"),
+            (500, "D"),
+            (100, "C"),
+            (50, "L"),
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I")
+        };
+
+        public string ToRomanNumeral(int numeral)
+        {
+            string romanNumeralString = "";
+
+            foreach (var symbol in _romanSymbolsList)
+            {
+                while (numeral >= symbol.Number)
+                {
+                    romanNumeralString += symbol.RomanSymbol;
+                    numeral -= symbol.Number;
+                }
+            }
+
+            return romanNumeralString;
+        }
     }
 }
---
+++ NumeralConverterTests/NumeralConverterTests.cs
@@ -0,0 +1,45 @@
+﻿using NumeralConverterLib;
+
+namespace NumeralConverterTests
+{
+    [TestFixture]
+    internal class NumeralConverterTests
+    {
+        private NumeralConverter _numeralConverter;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _numeralConverter = new NumeralConverter();
+        }
+
+        [TestCase(1, ExpectedResult = "I")]
+        [TestCase(2, ExpectedResult = "II")]
+        [TestCase(3, ExpectedResult = "III")]
+        [TestCase(4, ExpectedResult = "IV")]
+        [TestCase(5, ExpectedResult = "V")]
+        [TestCase(6, ExpectedResult = "VI")]
+        [TestCase(9, ExpectedResult = "IX")]
+        [TestCase(10, ExpectedResult = "X")]
+        [TestCase(12, ExpectedResult = "XII")]
+        [TestCase(20, ExpectedResult = "XX")]
+        [TestCase(21, ExpectedResult = "XXI")]
+        [TestCase(50, ExpectedResult = "L")]
+        [TestCase(67, ExpectedResult = "LXVII")]
+        [TestCase(100, ExpectedResult = "C")]
+        [TestCase(300, ExpectedResult = "CCC")]
+        [TestCase(500, ExpectedResult = "D")]
+        [TestCase(600, ExpectedResult = "DC")]
+        [TestCase(1000, ExpectedResult = "M")]
+        [TestCase(1668, ExpectedResult = "MDCLXVIII")]
+        [TestCase(2291, ExpectedResult = "MMCCLXXXXI")]
+        public string ConvertToRoman_WhenGivenANumeral_ThenReturnTheNumeralAsRomanString(int numeral)
+        {
+            //Arrange and Act
+            string romanNumeral = _numeralConverter.ToRomanNumeral(numeral);
+
+            // Assert
+            return romanNumeral;
+        }
+    }
+}
